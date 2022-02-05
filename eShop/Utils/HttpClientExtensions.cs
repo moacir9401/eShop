89a06319687dd10389ae4345017081008e16593d
Moacir Afonso <36249463+moacir9401@users.noreply.github.com>
2022-02-05 11:07:58 -0300

    adicionando ao httpClienteExtension o PostAsJson e PutAsJson

--- eShop/Utils/HttpClientExtensions.cs
+++ eShop/Utils/HttpClientExtensions.cs
@@ -1,9 +1,14 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace eShop.Utils
 {
     public static class HttpClientExtensions
     {
+
+        private static MediaTypeHeaderValue contentType 
+            = new MediaTypeHeaderValue("application/json");
+
         public static async Task<T> ReadContextAs<T>(this HttpResponseMessage response)
         {
             if (!response.IsSuccessStatusCode) throw
@@ -15,6 +20,30 @@ namespace eShop.Utils
             return JsonSerializer.Deserialize<T>(dataAsString,
                 new JsonSerializerOptions
                 { PropertyNameCaseInsensitive = true });
+
+        }
+
+        public static Task<HttpResponseMessage> PostAsJson<T>(
+            this HttpClient httpClient,
+            string url,
+            T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PostAsync(url, content);
+
+        }
+
+        public static Task<HttpResponseMessage> PutAsJson<T>(
+          this HttpClient httpClient,
+          string url,
+          T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PutAsync(url, content);
 
         }
     }
