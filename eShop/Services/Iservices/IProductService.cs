105a2af4dc58849b6ea4a26215fbf224b7f2945f
Moacir Afonso <36249463+moacir9401@users.noreply.github.com>
2022-02-05 10:57:59 -0300

    criando Iservices do produto e implementado a classe httpClientExtension

---
+++ eShop/Services/Iservices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Models;
+
+namespace eShop.Services.Iservices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProduct();
+        Task<ProductModel> FindProductByID(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
