bdaefddb0962a1c1cff8969acbf4fb19da2b301a
Moacir Afonso <36249463+moacir9401@users.noreply.github.com>
2022-02-06 00:26:12 -0300

    implementando o identity server do duende software

---
+++ eshop.IdentityServer/Model/Context/MYSQLContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eshop.IdentityServer.Model.Context
+{
+    public class MySqlContext : IdentityDbContext<ApplicationUser>
+    {
+        public MySqlContext() { }
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+    }
+}
