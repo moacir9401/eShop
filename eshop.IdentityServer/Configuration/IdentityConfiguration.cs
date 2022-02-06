c037a1e5e24f96d13d88a401583b5dc3e33961e9
Moacir Afonso <36249463+moacir9401@users.noreply.github.com>
2022-02-06 00:40:11 -0300

    implementando a classe IdentityConfiguration

---
+++ eshop.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace eshop.IdentityServer.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> apiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("eshop_shopping", "eshopShopping Server"),
+                new ApiScope(name: "read", "Read data."),
+                new ApiScope(name: "Write", "Write data."),
+                new ApiScope(name: "Delete", "Delete data."),
+            };
+
+        public static IEnumerable<Client> clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"read","write","profile" }
+                }
+            };
+    }
+}
