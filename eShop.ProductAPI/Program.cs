917bdec39b00b087df1bcdbe86edf2adeb9d81ed
Moacir Afonso <36249463+moacir9401@users.noreply.github.com>
2022-02-06 15:16:47 -0300

    adicionando autenticação no swagger

--- eShop.ProductAPI/Program.cs
+++ eShop.ProductAPI/Program.cs
@@ -4,6 +4,7 @@ using eShop.ProductAPIs.Config;
 using eShop.ProductAPIs.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +20,7 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddDbContext<MySqlContext>(options => options
 .UseMySql(connection,
 new MySqlServerVersion
-(new Version(10,4,21))));
+(new Version(10, 4, 21))));
 
 builder.Services.AddControllers();
 
@@ -44,7 +45,36 @@ builder.Services.AddAuthorization(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oath2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
