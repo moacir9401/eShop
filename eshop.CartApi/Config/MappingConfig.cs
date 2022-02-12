ad625e1140a1bb0722d621fd76adf8da85c6d789
Moacir <moacir9401@gmail.com>
2022-02-12 14:45:16 -0300

    configurando program.cs

---
+++ eshop.CartApi/Config/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper; 
+using eShop.CartAPI.Model;
+
+namespace eShop.CartAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<ProductVO, Product>();
+                //config.CreateMap<Product, ProductVO>();
+            });
+
+            return mappingConfig;
+        }
+
+    }
+}
---
+++ eshop.CartApi/Model/Context/MySqlContext.cs
@@ -0,0 +1,13 @@
+﻿using eShop.ProductAPIs.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.CartAPI.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext() { }
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+        public DbSet<Product> products { get; set; }
+         
+    }
+}
